d8e80334d469a721adc35463521245976f96fbcd
iwandev23 <129351341+iwandev23@users.noreply.github.com>
2023-07-15 20:02:37 +0700

    upload file utama

---
+++ detail_produk.php
@@ -0,0 +1,250 @@
+<?php 
+include"template/connect_main.php";
+$idp=$_GET['idp'];
+$sql="SELECT
+                  id,
+                  nama_produk,
+                  kategori,
+                  foto,
+                  foto2,
+                  foto3,
+                  harga_total,
+                  `status`
+                FROM produk where id='$idp'";
+        $query=mysqli_query($link,$sql);
+        list($id,$nama_produk,$kategori,$images,$images2,$images3,$harga,$status)=mysqli_fetch_array($query);	
+include ("template/header_produk.php");?>
+<?php include("template/navbar.php"); ?>
+	<style>
+		.btn-warna{
+			width:32px;
+			height:32px;
+			border: 1px solid #8d8d8d;
+		}
+		@media (max-width: 767px){
+			
+			.img-indikator{
+				display:none;
+			}
+			.carousel-indicators{
+				display:none;
+			}
+		}
+		.container_login {
+			display: flex;
+			flex-direction: row;
+			width: 100%;
+		}
+
+		.login-separator {
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			background-image: #fff;
+			padding: 30px 0;
+			min-height: 110px;
+			width: 100%;
+		}
+	</style>
+
+<section id="content" style="margin-bottom: 0px; ">
+
+	<div class="content-wrap" style="background-image: url('assets/images/img_bg.jpg') !important">
+		<div id="title-mob" class="container mb-2">
+			<!-- <a href="#">Kerudung Sekolah /</a> -->
+			<a href="#"><h5><?php echo $nama_produk;?></h5></a>
+		</div>
+		<div id="mobile" class="container clearfix" style="background-color:#fff; border-radius:8px;box-shadow: 0px 0px 2px 1px rgba(128, 0, 0, 0.25);">
+
+			<!-- Post Content
+			============================================= -->
+			<div class="row">
+				<div class="col-md-6" >
+					<div id="indikator" class="carousel slide" data-ride="carousel" data-interval="false">
+						<div class="carousel-inner">
+							<div class="carousel-item active">
+								<img id="img-preview" src="<?php echo $images;?>">
+							</div>
+							<!-- <?php if ($image1!=''){?>
+								<div class="carousel-item active">
+									<img id="img-preview" src="assets/foto_produk/<?php echo $image1; ?>">
+								</div>
+							<?php }
+
+							if ($image2!=''){?>
+								<div class="carousel-item">
+									<img id="img-preview" src="assets/foto_produk/<?php echo $image2; ?>">
+								</div>
+							<?php }
+							if ($image3!=''){?>
+								<div class="carousel-item">
+									<img id="img-preview" src="assets/foto_produk/<?php echo $image3; ?>">
+								</div>
+							<?php }
+							if ($image4!=''){?>
+								<div class="carousel-item">
+									<img id="img-preview" src="assets/foto_produk/<?php echo $image4; ?>">
+								</div>
+							<?php }?> -->
+						</div>
+						<ol class="carousel-indicators ">
+							<li data-target="#indikator" data-slide-to="0" class="active">
+								<img class="img-indikator" src="<?php echo $images?>">
+							</li>
+							
+							<?php if($images2!=''){?>
+							<li data-target="#indikator" data-slide-to="0" class="active">
+								<img class="img-indikator" src="<?php echo $images2?>">
+							</li>
+							<?php 
+							}
+
+							if($images3!=''){
+							?>
+							<li data-target="#indikator" data-slide-to="0" class="active">
+								<img class="img-indikator" src="<?php echo $images3?>">
+							</li>
+							<?php } ?>
+							<!-- <?php if ($image1!=''){?>
+								<li data-target="#indikator" data-slide-to="0" class="active"><img class="img-indikator" src="assets/foto_produk/<?= $image1 ?>">-</li>
+							<?php }
+
+							if ($image2!=''){?>
+								<li data-target="#indikator" data-slide-to="1" class="active"><img class="img-indikator" src="assets/foto_produk/<?= $image2 ?>">-</li>
+							<?php }
+							if ($image3!=''){?>
+								<li data-target="#indikator" data-slide-to="2"><img class="img-indikator" src="assets/foto_produk/<?= $image3 ?>">-</li>
+							<?php }
+							if ($image4!=''){?>
+								<li data-target="#indikator" data-slide-to="3"><img class="img-indikator" src="assets/foto_produk/<?= $image4 ?>">-</li>
+							<?php }?> -->
+							
+							
+							
+						</ol>
+					</div>
+				</div>
+			
+				<div id="mob-produk" class="col detail-p">
+					<h3 class="margin0" style="font-size: 20px; margin-top:16px"><?php echo $nama_produk;?></h3>
+					<!-- <p class="margin0 hilang"><?php echo $persen_disc ;?></p> -->
+					<h1 class="rp" id="show_price"><?php echo number_format($harga);?></h1>
+					<input type="hidden" name="harga1" id="harga1">
+					<!-- <p class="margin0">50% <del>Rp.100.000</del></p> -->
+
+					<p class="pilih">Size Available</p>
+					<div id="size_choise">
+							<?php 
+							$sql="SELECT
+								    u.ukuran
+								FROM
+								    produk_detail AS pd
+								    INNER JOIN ukuran AS u 
+								        ON (pd.ukuran = u.kode_ukuran) WHERE pd.id_produk='$idp'
+								        GROUP BY u.ukuran";
+							 $query=mysqli_query($link,$sql);	
+							 //echo $sql;        
+							while(list($ukuran)=mysqli_fetch_array($query)){    	        
+							?>
+							<div class="button" >
+								<input type="radio" id="<?php echo $ukuran;?>" name="size"  value="<?php echo $ukuran;?>"  />
+								<label class="btn btn-outline-secondary size" for="XS"><?php echo $ukuran; ?></label>
+							</div>
+							<?php } ?>
+
+					</div>
+					<div id="tampil_ukuran">
+						
+					</div>
+					
+					<p class="pilih">Color Available</p>
+					<div>
+					<?php 
+					$sql="SELECT
+						    pd.warna
+						    , w.warna
+						FROM
+						    produk_detail AS pd
+						    INNER JOIN warna AS w 
+						        ON (pd.warna = w.kode_warna)WHERE pd.id_produk='$idp' group by pd.warna";
+					 $query=mysqli_query($link,$sql);	
+							 //echo $sql;        
+							while(list($kode_warna,$warna)=mysqli_fetch_array($query)){    	      	        
+					?>
+							<div class="button" >
+								<input type="radio" id="<?php echo $warna;?>" name="size"  value="<?php echo $warna;?>"  />
+								<label class="btn btn-outline-secondary size" for="XS"><?php echo $warna; ?></label>
+							</div>
+							
+					<?php } ?>		
+							<p>&nbsp;</p>
+							<!-- <div class="button btn-warna" style="background-color: #805859 !important;margin-right:5px!important;">
+							
+							</div>
+							<div class="button btn-warna" style="background-color: #564a40 !important;margin-right:5px!important;">
+								
+							</div>
+							<div class="button btn-warna" style="background-color: #424240 !important;margin-right:5px!important;">
+							
+							</div>
+							<div class="button btn-warna" style="background-color: #2c2a40 !important;margin-right:5px!important;">
+							
+							</div>
+							<div class="button btn-warna" style="background-color: #0c0a0d !important;margin-right:5px!important;">
+								
+							</div>
+							<div class="button btn-warna" style="background-color: #eeecf7 !important;margin-right:5px!important;">
+								
+							</div> -->
+						
+						
+					</div>
+
+					
+				</div>
+			</div>
+			<!-- .postcontent end -->
+			<br>
+		</div>
+
+		<div id="mobile" class="container clearfix mt-2" style="background-color:#fff;box-shadow: 0px 0px 2px 1px rgba(128, 0, 0, 0.25); border-radius:8px ">
+
+			<!-- Post Content
+			============================================= -->
+			<div class="row">
+				<div id="mob-produk" class="col mt-3">
+					<!-- <h4 style="margin-bottom: 0px !important;">Spesifikasi Produk</h4>
+					<p style="margin-bottom: 0px !important;">Bahan: Katun</p> -->
+					
+					<p class="pilih">Product Description</p>
+					<div class="row mt-2 mb-2">
+					<!-- <div class="col-2">Fabric</div>
+					<div class="col">: Camila</div> -->
+					</div>
+					<div class="row mt-2 mb-2">
+					<div class="col-2">Category</div>
+					<div class="col">: <?php echo $kategori;?></div>
+					</div>
+					<p style="margin-bottom: 0px !important;">
+						<?php echo $nama_produk;?>
+					</p>
+				</div>
+			</div>
+			<!-- .postcontent end -->
+			<br>
+		</div>
+	</div>
+
+	<!-- Button trigger modal -->
+
+
+<section id="separator" style="margin-bottom: 0; ">
+    <div class="container_login">
+        <div class="login-separator">
+            <img class="img-brand" src="assets/images/img_family_brand.png" alt="">
+        </div>
+    </div>
+</section>
+<?php include ("template/footer.php");?>
+<script type="text/javascript" src="assets/js/detailproduk.js"></script>
+<script src="assets/js/format.number.min.js"></script>	
---
+++ index.php
@@ -0,0 +1,402 @@
+<?php include("template/header.php"); ?>
+<?php include("template/navbar.php"); ?>
+<style>
+  .img-fluid {
+    width: 100%;
+    height: 338px !important;
+    border-radius: 8px;
+    
+  }
+  .breadcrumb-item + .breadcrumb-item::before {
+    display: inline-block;
+    color: #6c757d;
+    content: none !important;
+    
+  }
+  .breadcrumb a {
+    font-size:16px !important;
+    font-family: 'Inter Tight', sans-serif;
+    font-size: 14px;
+    color: #6a6a6a;
+    font-weight: 600;
+  }
+  .breadcrumb li {
+  text-align:center;
+  padding-left:30px !important;
+  }
+
+  p.jenis-kategori{
+    font-family: 'Roboto', sans-serif;
+    font-size: 24px;
+    font-weight: bold;
+    color: #525252;
+  }
+  .aktif{
+    color:#b75ae2 !important;
+  }
+  .garis{
+    margin-top:8px;
+    width:85px;
+    display:block;
+    margin-left: 4px;
+    
+  }
+  .link-dropdown:active{
+    background-color:#fff;
+    color: #b75ae2;
+    font-weight:300px;
+  }
+  .link-dropdown:hover{
+    background-color:#fff;
+    color: #b75ae2;
+    font-weight:300px;
+  }
+  .dropdown-item:hover, .dropdown-item:focus {
+    
+    background-color: #fff !important;
+  }
+  .btn-outline-dropdown:focus{
+    background-color: #b75ae2 !important;
+    box-shadow: 0 0 0 0.2rem  #b75ae2 !important;
+    color:#fff !important;
+  }
+.container_login {
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+}
+
+.login-separator {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-image: #fff;
+    padding: 30px 0;
+    min-height: 110px;
+    width: 100%;
+}
+
+.text-title{
+  font-family: Roboto, sans-serif;
+  font-size: 24px;
+  font-weight: bold;
+  font-stretch: condensed;
+  font-style: normal;
+  line-height: 1;
+  letter-spacing: normal;
+  text-align: left;
+  color: #525252;
+}
+
+.link-category {
+  font-family: Roboto, sans-serif;
+  font-size: 14px;
+  font-weight: 400;
+  font-stretch: normal;
+  font-style: normal;
+  color: #6a6a6a;
+  float: right;
+}
+
+.color-product{
+  color: #525252!important;
+  font-size: 16px;
+  font-weight: 500;
+}
+@media (max-width: 767px){
+    .img-fluid {
+      height:200px !important;
+    }
+    .breadcrumb a {
+      font-size:12px !important;
+      font-family: 'Inter Tight', sans-serif;
+      color: #6a6a6a;
+      font-weight: 400;
+    }
+    .breadcrumb li {
+    text-align:center;
+    margin-left:10px !important;
+    height: 30px;
+    }
+
+    p.jenis-kategori{
+      font-family: 'Roboto', sans-serif;
+      font-size: 16px;
+      font-weight: bold;
+      color: #525252;
+    }
+    .btn-outline-dropdown{
+      font-size:12px !important;
+      font-weight: 400 !important;
+    }
+    .garis{
+      width:40px;
+      margin-left: 0px;
+    }
+    .breadcrumb-item + .breadcrumb-item {
+      padding-left: 1.2rem !important;
+    }
+    .text-title{
+      margin-left:10px;
+      font-size:16px;
+    }
+    .link-category{
+      margin-right:10px;
+      font-size:12px;
+    }
+    .col-md-4 {
+      padding-right: 8px !important;
+      padding-left: 8px !important;
+      flex-basis: 0;
+      flex-grow: 1;
+    }
+    .card-img-top{
+
+      height:auto;
+    }
+    .img-brand {
+      width:80%;
+    }
+    .card{
+				min-width: 120px;
+				min-height: 255px;
+				max-width: 300px;
+				
+				width: auto;
+				height: auto;
+			}
+  }
+  @media (max-width:500px){
+    .img-fluid {
+      height:200px !important;
+    }
+    .breadcrumb a {
+      font-size:12px !important;
+      font-family: 'Inter Tight', sans-serif;
+      color: #6a6a6a;
+      font-weight: 400;
+    }
+    .breadcrumb li {
+    text-align:center;
+    margin-left:10px !important;
+    height: 30px;
+    }
+
+    p.jenis-kategori{
+      font-family: 'Roboto', sans-serif;
+      font-size: 16px;
+      font-weight: bold;
+      color: #525252;
+    }
+    .btn-outline-dropdown{
+      font-size:12px !important;
+      font-weight: 400 !important;
+    }
+    .garis{
+      width:40px;
+      margin-left: 0px;
+    }
+    .breadcrumb-item + .breadcrumb-item {
+      padding-left: 1.2rem !important;
+    }
+    .text-title{
+      margin-left:10px;
+      font-size:16px;
+    }
+    .link-category{
+      margin-right:10px;
+      font-size:12px;
+    }
+    .col-md-4 {
+      padding-right: 8px !important;
+      padding-left: 8px !important;
+      flex-basis: 0;
+      flex-grow: 1;
+    }
+    .card-img-top{
+
+      height:auto;
+    }
+    .img-brand {
+      width:80%;
+    }
+  }
+  @media (max-width:411px){
+    .img-fluid {
+      height: 146px !important;
+    }
+    .breadcrumb {
+      flex-wrap: nowrap;
+    }
+    .breadcrumb a {
+      font-size:12px !important;
+      font-family: 'Inter Tight', sans-serif;
+      color: #6a6a6a;
+      font-weight: 400;
+    }
+    .breadcrumb li {
+    text-align:center;
+    margin-left:0px !important;
+    }
+
+    p.jenis-kategori{
+      font-family: 'Roboto', sans-serif;
+      font-size: 14px;
+      font-weight: bold;
+      color: #525252;
+    }
+    .btn-outline-dropdown{
+      font-size:11.5px !important;
+      font-weight: 400 !important;
+    }
+    .garis{
+      width:40px;
+      margin-left: 0px;
+    }
+    .breadcrumb-item + .breadcrumb-item {
+      padding-left: 1.2rem !important;
+    }
+    .boxed-slider{
+      padding-top: 8px !important;
+    }
+    .box-slide{
+      padding-left: 8px !important;
+      padding-right: 8px !important;
+    }
+    .content-wrap {
+      margin-top: -70px;
+    }
+    .text-title{
+      margin-left:10px;
+      font-size:16px;
+    }
+    .link-category{
+      margin-right:10px;
+      font-size:12px;
+    }
+    .col-md-4 {
+      padding-right: 8px !important;
+      padding-left: 8px !important;
+      flex-basis: 0;
+      flex-grow: 1;
+    }
+    .card-img-top{
+
+      height:auto;
+    }
+    .img-brand {
+      width:80%;
+    }
+    .color-product{
+      font-size:9px;
+    }
+    .card{
+				min-width: 100px;
+				/* max-height: 255px; */
+				max-width: 100px;
+				
+				width: auto;
+				height: auto;
+			}
+  }
+</style>
+<section id="slider" class="slider-element boxed-slider" style="background-image: url('assets/images/img_bg.jpg')">
+  <div id="mobile-slider" class="container clearfix box-slide">
+      <img src="assets/images/img_banner.png" class="img-fluid" alt="Responsive image">
+      <div class="row">
+      <div class="col" style="margin-top:20px;">
+        <h2 class="text-title">Palestine Series</h2>
+      </div>
+      <div class="col" style="margin-top:20px;">
+        <a href="category.php?cate=all" class="link-category">Show all</a>
+      </div>
+      </div>
+  </div>
+</section>
+<section id="content" style="margin-bottom: 0px;background: #f5f5f5; ">
+  <div class="container-fluid" style="background-color:#f5f5f5">
+
+    <div id="mobile" class="container" >
+
+      <div class="row pt-4">
+        <?php 
+        $sql="SELECT
+                id,
+                nama_kategori,
+                images,
+                seq,
+                updatedate,
+                updateby,
+                `status`
+              FROM produk_kategori order by seq asc";
+        $query=mysqli_query($link,$sql);
+        while(list($id,$nama_kategori,$images,$seq,$updatedate,$updateby,$status)=mysqli_fetch_array($query)){      
+        ?>
+        <div class="col-md-4 pt-2">
+          <a href="category.php?cate=<?php echo $id;?>">
+            <div class="card">
+              <img class="card-img-top" src="assets/images/<?php echo $images;?>" alt="Card image cap">
+              <div class="card-body">
+                <h5 class="card-title"><?php echo $nama_kategori?></h5>
+                <p class="card-text">For Your All Activity</p>
+              </div>
+            </div>
+            </a>
+        </div>
+        <?php } ?>
+      </div>
+
+
+    </div>
+    <div id="mobile" class="container mt-4" >
+
+      <h1 class="pt-4">New Arrivals</h1>
+      <div class="row">
+        <?php 
+          $sql="SELECT
+                  id,
+                  nama_produk,
+                  kategori,
+                  foto,
+                  harga_total,
+                  `status`
+                FROM produk";
+        $query=mysqli_query($link,$sql);
+        while(list($id,$nama_produk,$kategori,$images,$harga,$status)=mysqli_fetch_array($query)){    
+        ?>
+        <div class="col-md-4 pt-4">
+          <a href="detail_produk.php?idp=<?php echo $id?>">
+            <div class="card">
+              <img class="card-img-top" src="<?php echo $images; ?>" alt="<?php $nama_produk;?>">
+              <div class="card-body">
+                <h5 class="card-title"><?php echo $nama_produk;?>
+                  <br />
+                  <!-- <span class="color-product">Black</span> -->
+                </h5>
+                <p class="harga card-text"><?php echo number_format($harga); ?></p>
+              </div>
+            </div>
+          </a>
+        </div>
+        <?php } ?>
+        
+       
+      </div>
+
+
+    </div>
+    <div class="container mt-5">
+
+    </div>
+  </div>
+</section>
+
+<section id="separator" style="margin-bottom: 0; ">
+    <div class="container_login">
+        <div class="login-separator">
+            <img class="img-brand" src="assets/images/img_family_brand.png" alt="">
+        </div>
+    </div>
+</section>
+
+<?php include("template/footer.php"); ?>
