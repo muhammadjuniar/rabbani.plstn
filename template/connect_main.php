fcb0598451de422d7b65b53bea47a0b3478a11ad
iwandev23 <129351341+iwandev23@users.noreply.github.com>
2023-07-15 20:07:03 +0700

    upload template

---
+++ template/connect_main.php
@@ -0,0 +1,9 @@
+<?php 
+         $host="156.67.212.223";  
+		 $admin="rabbanico_dev";
+		 $password="?MK9v3YhNJaD@kw";
+		 $database="rabbanico_palestine";
+													  //==================
+		 $link=mysqli_connect($host,$admin,$password,$database)or die("Belum Terkoneksi");
+
+?>
---
+++ template/header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en-US">
+<head>
+    <?php include"connect_main.php";?>
+   	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta name="author" content="SemiColonWeb" />
+
+	<!-- Stylesheets
+	============================================= -->
+	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,400i,700|Raleway:300,400,500,600,700|Crete+Round:400i" rel="stylesheet" type="text/css" />
+  <!-- <link href="https://fonts.googleapis.com/css2?family=Inter+Tight:wght@200&family=Roboto:ital,wght@0,300;0,400;1,100&display=swap" rel="stylesheet">  -->
+	
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@700&display=swap" rel="stylesheet"> 
+  	<link rel="stylesheet" href="assets/css/bootstrap.css" type="text/css" />
+	<link rel="stylesheet" href="assets/style.css" type="text/css" />
+	<link rel="stylesheet" href="assets/css/dark.css" type="text/css" />
+	<link rel="stylesheet" href="assets/css/font-icons.css" type="text/css" />
+	<link rel="stylesheet" href="assets/css/animate.css" type="text/css" />
+	<link rel="stylesheet" href="assets/css/magnific-popup.css" type="text/css" />
+  
+	<link rel="stylesheet" href="assets/css/responsive.css" type="text/css" />
+	<link rel="stylesheet" href="assets/new_style.css" type="text/css" />
+	<link rel="stylesheet" href="assets/new_style_responsive.css" type="text/css" />
+		<!-- External JavaScripts
+	============================================= -->
+	<script src="assets/js/jquery.js"></script>
+	<script src="assets/js/plugins.js"></script>
+	
+
+	<!-- Footer Scripts
+	============================================= -->
+	<script src="assets/js/functions.js"></script>
+	<script src="assets/js/script.js"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	
+
+	<!-- Document Title
+	============================================= -->
+	<title>plstn.rabbani.id</title>
+	<link rel="icon" type="image/x-icon" href="assets/images/favicon/logo_rabbani.png">
+</head>
+<body class="stretched no-transition">
