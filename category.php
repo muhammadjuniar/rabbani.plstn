341231ace27c6320dcda3fa60e97a829711f6014
juniar <muhammadjuniar77@gmail.com>
2023-07-17 15:18:12 +0700

    restore update responsive

--- category.php
+++ category.php
@@ -1,6 +1,304 @@
 <?php include("template/header.php"); ?>
 <?php include("template/navbar.php"); ?>
 <style>
+  .img-fluid {
+    width: 100%;
+    height: 338px !important;
+    border-radius: 8px;
+    
+  }
+  .breadcrumb-item + .breadcrumb-item::before {
+    display: inline-block;
+    color: #6c757d;
+    content: none !important;
+    
+  }
+  .breadcrumb a {
+    font-size:16px !important;
+    font-family: 'Inter Tight', sans-serif;
+    font-size: 14px;
+    color: #6a6a6a;
+    font-weight: 600;
+  }
+  .breadcrumb li {
+  text-align:center;
+  padding-left:30px !important;
+  }
+
+  p.jenis-kategori{
+    font-family: 'Roboto', sans-serif;
+    font-size: 24px;
+    font-weight: bold;
+    color: #525252;
+  }
+  .aktif{
+    color:#b75ae2 !important;
+  }
+  .garis{
+    margin-top:8px;
+    width:85px;
+    display:block;
+    margin-left: 4px;
+    
+  }
+  .link-dropdown:active{
+    background-color:#fff;
+    color: #b75ae2;
+    font-weight:300px;
+  }
+  .link-dropdown:hover{
+    background-color:#fff;
+    color: #b75ae2;
+    font-weight:300px;
+  }
+  .dropdown-item:hover, .dropdown-item:focus {
+    
+    background-color: #fff !important;
+  }
+  .btn-outline-dropdown:focus{
+    background-color: #b75ae2 !important;
+    box-shadow: 0 0 0 0.2rem  #b75ae2 !important;
+    color:#fff !important;
+  }
+.container_login {
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+}
+
+.login-separator {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-image: #fff;
+    padding: 30px 0;
+    min-height: 110px;
+    width: 100%;
+}
+
+.text-title{
+  font-family: Roboto, sans-serif;
+  font-size: 24px;
+  font-weight: bold;
+  font-stretch: condensed;
+  font-style: normal;
+  line-height: 1;
+  letter-spacing: normal;
+  text-align: left;
+  color: #525252;
+}
+
+.link-category {
+  font-family: Roboto, sans-serif;
+  font-size: 14px;
+  font-weight: 400;
+  font-stretch: normal;
+  font-style: normal;
+  color: #6a6a6a;
+  float: right;
+}
+
+.color-product{
+  color: #525252!important;
+  font-size: 16px;
+  font-weight: 500;
+}
+@media (max-width: 767px){
+    .img-fluid {
+      height:200px !important;
+    }
+    .breadcrumb a {
+      font-size:12px !important;
+      font-family: 'Inter Tight', sans-serif;
+      color: #6a6a6a;
+      font-weight: 400;
+    }
+    .breadcrumb li {
+    text-align:center;
+    margin-left:10px !important;
+    height: 30px;
+    }
+
+    p.jenis-kategori{
+      font-family: 'Roboto', sans-serif;
+      font-size: 16px;
+      font-weight: bold;
+      color: #525252;
+    }
+    .btn-outline-dropdown{
+      font-size:12px !important;
+      font-weight: 400 !important;
+    }
+    .garis{
+      width:40px;
+      margin-left: 0px;
+    }
+    .breadcrumb-item + .breadcrumb-item {
+      padding-left: 1.2rem !important;
+    }
+    .text-title{
+      margin-left:10px;
+      font-size:16px;
+    }
+    .link-category{
+      margin-right:10px;
+      font-size:12px;
+    }
+    .col-md-4 {
+      padding-right: 8px !important;
+      padding-left: 8px !important;
+      flex-basis: 0;
+      flex-grow: 1;
+    }
+    .card-img-top{
+
+      height:auto;
+    }
+    .img-brand {
+      width:80%;
+    }
+    .card{
+				min-width: 120px;
+				min-height: 255px;
+				max-width: 300px;
+				
+				width: auto;
+				height: auto;
+			}
+  }
+  @media (max-width:500px){
+    .img-fluid {
+      height:200px !important;
+    }
+    .breadcrumb a {
+      font-size:12px !important;
+      font-family: 'Inter Tight', sans-serif;
+      color: #6a6a6a;
+      font-weight: 400;
+    }
+    .breadcrumb li {
+    text-align:center;
+    margin-left:10px !important;
+    height: 30px;
+    }
+
+    p.jenis-kategori{
+      font-family: 'Roboto', sans-serif;
+      font-size: 16px;
+      font-weight: bold;
+      color: #525252;
+    }
+    .btn-outline-dropdown{
+      font-size:12px !important;
+      font-weight: 400 !important;
+    }
+    .garis{
+      width:40px;
+      margin-left: 0px;
+    }
+    .breadcrumb-item + .breadcrumb-item {
+      padding-left: 1.2rem !important;
+    }
+    .text-title{
+      margin-left:10px;
+      font-size:16px;
+    }
+    .link-category{
+      margin-right:10px;
+      font-size:12px;
+    }
+    .col-md-4 {
+      padding-right: 8px !important;
+      padding-left: 8px !important;
+      flex-basis: 0;
+      flex-grow: 1;
+    }
+    .card-img-top{
+
+      height:auto;
+    }
+    .img-brand {
+      width:80%;
+    }
+  }
+  @media (max-width:411px){
+    .img-fluid {
+      height: 146px !important;
+    }
+    .breadcrumb {
+      flex-wrap: nowrap;
+    }
+    .breadcrumb a {
+      font-size:12px !important;
+      font-family: 'Inter Tight', sans-serif;
+      color: #6a6a6a;
+      font-weight: 400;
+    }
+    .breadcrumb li {
+    text-align:center;
+    margin-left:0px !important;
+    }
+
+    p.jenis-kategori{
+      font-family: 'Roboto', sans-serif;
+      font-size: 14px;
+      font-weight: bold;
+      color: #525252;
+    }
+    .btn-outline-dropdown{
+      font-size:11.5px !important;
+      font-weight: 400 !important;
+    }
+    .garis{
+      width:40px;
+      margin-left: 0px;
+    }
+    .breadcrumb-item + .breadcrumb-item {
+      padding-left: 1.2rem !important;
+    }
+    .boxed-slider{
+      padding-top: 8px !important;
+    }
+    .box-slide{
+      padding-left: 8px !important;
+      padding-right: 8px !important;
+    }
+    .content-wrap {
+      margin-top: -70px;
+    }
+    .text-title{
+      margin-left:10px;
+      font-size:16px;
+    }
+    .link-category{
+      margin-right:10px;
+      font-size:12px;
+    }
+    .col-md-4 {
+      padding-right: 8px !important;
+      padding-left: 8px !important;
+      flex-basis: 0;
+      flex-grow: 1;
+    }
+    .card-img-top{
+
+      height:auto;
+    }
+    .img-brand {
+      width:80%;
+    }
+    .color-product{
+      font-size:9px;
+    }
+    .card{
+				min-width: 100px;
+				/* max-height: 255px; */
+				max-width: 100px;
+				
+				width: auto;
+				height: auto;
+			}
+  }
 </style>
 <section id="slider" class="slider-element boxed-slider" style="background-image: url('assets/images/img_bg.jpg')">
   <div id="mobile-slider" class="container clearfix box-slide">
@@ -21,8 +319,7 @@
     
     <div id="mobile" class="container" >
 
-      <div class="badan pt-4">
-        <div class="row">
+      <div class="row pt-4">
         <?php 
         $sql="SELECT
                   id,
@@ -35,18 +332,24 @@
         $query=mysqli_query($link,$sql);
         while(list($id,$nama_produk,$kategori,$images,$harga,$status)=mysqli_fetch_array($query)){    
         ?>
-        <div class="list-produk">
-            <a href="detail_produk.php?idp=<?php echo $id?>">
-              <img src="<?php echo $images; ?>" alt="<?php $nama_produk;?>">
-  
-              <h5 class="title"><?php echo $nama_produk;?></h5>
-              <h5 class="harga-produk">IDR. <?php echo number_format($harga); ?></h5>
+        <div class="col-md-4 pt-2">
+          <a href="detail_produk.php?idp=<?php echo $id?>">
+            <div class="card">
+              <img class="card-img-top" src="<?php echo $images; ?>" alt="<?php $nama_produk;?>">
+              <div class="card-body">
+                <h5 class="card-title"><?php echo $nama_produk;?>
+                  <br />
+                  <!-- <span class="color-product">Black</span> -->
+                </h5>
+                <p class="harga card-text">IDR. <?php echo number_format($harga); ?></p>
+              </div>
+            </div>
             </a>
         </div>
         <?php } ?> 
 
        
-        </div>
+       
       </div>
 
 
--- detail_produk.php
+++ detail_produk.php
@@ -148,7 +148,7 @@ include ("template/header_produk.php");?>
 							?>
 							<div class="button" >
 								<input type="radio" id="<?php echo $ukuran;?>" name="size"  value="<?php echo $ukuran;?>"  />
-								<label class="btn btn-outline-secondary size" for="<?php echo $ukuran;?>"><?php echo $ukuran; ?></label>
+								<label class="btn btn-outline-secondary size" for="XS"><?php echo $ukuran; ?></label>
 							</div>
 							<?php } ?>
 
@@ -162,7 +162,7 @@ include ("template/header_produk.php");?>
 					<?php 
 					$sql="SELECT
 						    pd.warna
-						    , w.warna_english
+						    , w.warna
 						FROM
 						    produk_detail AS pd
 						    INNER JOIN warna AS w 
@@ -172,8 +172,8 @@ include ("template/header_produk.php");?>
 							while(list($kode_warna,$warna)=mysqli_fetch_array($query)){    	      	        
 					?>
 							<div class="button" >
-								<input type="radio" id="<?php echo $warna;?>" name="color"  value="<?php echo $warna;?>"  />
-								<label class="btn btn-outline-secondary size" for="<?php echo $warna;?>"><?php echo $warna; ?></label>
+								<input type="radio" id="<?php echo $warna;?>" name="size"  value="<?php echo $warna;?>"  />
+								<label class="btn btn-outline-secondary size" for="XS"><?php echo $warna; ?></label>
 							</div>
 							
 					<?php } ?>		
