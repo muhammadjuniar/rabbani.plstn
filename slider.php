dbbcef50f9003bce54b73ea81c8e80f3db3349da
juniar <muhammadjuniar77@gmail.com>
2023-07-18 14:48:49 +0700

    update silder kategory muncul sesuai dengan get['cate']

--- slider.php
+++ slider.php
@@ -3,10 +3,18 @@
       <img src="assets/images/img_banner.png" class="img-fluid" alt="Responsive image">
       <div class="row">
         <div class="col" style="margin-top:20px;margin-bottom:-10px;">
-            <h2 class="text-title">Palestine Series</h2>
+            <?php if(isset($_GET['cate'])){ ?> 
+                <h2 class="text-title">Palestine Series / <?php echo ucfirst($_GET['cate']); ?></h2>
+            <?php }else{ ?>
+                <h2 class="text-title">Palestine Series</h2>
+            <?php } ?>
         </div>
         <div class="col" style="margin-top:20px;margin-bottom:-10px;">
-            <a href="category.php?cate=all" class="link-category">Show all</a>
+            <?php if(isset($_GET['cate'])){ ?> 
+                <a class="link-category"><?php echo ucfirst($_GET['cate']); ?></a>
+            <?php }else{ ?>
+                <a href="category.php?cate=all" class="link-category">Show all</a>
+            <?php } ?>
         </div>
       </div>
   </div>
