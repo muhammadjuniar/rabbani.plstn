d455ddb3c99e395649deba8df1004bff581a1088
juniar <muhammadjuniar77@gmail.com>
2023-07-29 14:53:25 +0700

    change temp folder path

--- office/api_proses_v2.php
+++ office/api_proses_v2.php
@@ -51,7 +51,7 @@ $fullpathWarna = "https://rabbani.co.id/warna/".$namafileWarna;
 
 $server_path="https://rabbani.co.id/product/";
 
-$local_dir = "temp/";
+$local_dir = "../temp/";
 
 // images
 
@@ -273,7 +273,7 @@ if($jenis=='produk_entry'){
         $kirim_data=$RestData->insert_b64_foto_produk_rpos($datakirim);
         //print_r($kirim_data); die();   
         
-        unlink($uploadfile);
+        // unlink($uploadfile);
         if (trim($kirim_data)=='berhasil')
         {
           $media_url=$fullpath;
