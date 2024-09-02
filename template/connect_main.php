7fdf04a25039e40f3fd8ff6ded911e6242a08ea1
muhammadjuniar <muhammadjuniar77@gmail.com>
2024-09-02 15:56:36 +0700

    update: connection

--- template/connect_main.php
+++ template/connect_main.php
@@ -1,7 +1,10 @@
 <?php 
-         $host="156.67.212.223";  
-		 $admin="rabbanico_dev";
-		 $password="?MK9v3YhNJaD@kw";
+        //  $host="156.67.212.223";  
+		//  $admin="rabbanico_dev";
+		//  $password="?MK9v3YhNJaD@kw";
+		$host = '206.189.43.33';
+		$admin = 'user_Rabbanico';
+		$password = 'ghdsfqw9e$sjfet^hfdg';
 		 $database="rabbanico_palestine";
 													  //==================
 		 $link=mysqli_connect($host,$admin,$password,$database)or die("Belum Terkoneksi");
