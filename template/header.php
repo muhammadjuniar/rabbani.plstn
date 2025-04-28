f5bf63083914f2eda04eb1e81bdbd834b347f541
muhammadjuniar <muhammadjuniar77@gmail.com>
2025-04-28 17:00:25 +0700

    add: google tag

--- template/header.php
+++ template/header.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html dir="ltr" lang="en-US">
+
 <head>
-    <?php include"connect_main.php";?>
+	<?php include "connect_main.php"; ?>
 	<!-- SEO Package -->
 	<meta name="title" content="Rabbani Palestine">
 	<meta name="description" content="Product catalog Rabbani with Palestinian theme. From Rabbani to Palestine.">
@@ -15,31 +16,45 @@
 	<!-- Stylesheets
 	============================================= -->
 	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,400i,700|Raleway:300,400,500,600,700|Crete+Round:400i" rel="stylesheet" type="text/css" />
-  	<!-- <link href="https://fonts.googleapis.com/css2?family=Inter+Tight:wght@200&family=Roboto:ital,wght@0,300;0,400;1,100&display=swap" rel="stylesheet">  -->
-	
-	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@700&display=swap" rel="stylesheet"> 
-  	<link rel="stylesheet" href="assets/css/bootstrap_new.css" type="text/css" />
+	<!-- <link href="https://fonts.googleapis.com/css2?family=Inter+Tight:wght@200&family=Roboto:ital,wght@0,300;0,400;1,100&display=swap" rel="stylesheet">  -->
+
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@700&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="assets/css/bootstrap_new.css" type="text/css" />
 	<link rel="stylesheet" href="assets/style.css" type="text/css" />
 	<link rel="stylesheet" href="assets/css/font-icons.css" type="text/css" />
-  
+
 	<link rel="stylesheet" href="assets/new_style.css" type="text/css" />
 	<link rel="stylesheet" href="assets/style_responsive.css" type="text/css" />
-		<!-- External JavaScripts
+	<!-- External JavaScripts
 	============================================= -->
 	<script src="assets/js/jquery.js"></script>
 	<script src="assets/js/plugins.js"></script>
-	
+
 
 	<!-- Footer Scripts
 	============================================= -->
 	<script src="assets/js/functions.js"></script>
 	<script src="assets/js/script.js"></script>
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	
+
 
 	<!-- Document Title
 	============================================= -->
 	<title>plstn.rabbani.id</title>
 	<link rel="icon" type="image/x-icon" href="assets/images/favicon/logo_rabbani.png">
+
+	<!-- Google tag (gtag.js) -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-SM51W52TLM"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+		gtag('js', new Date());
+
+		gtag('config', 'G-SM51W52TLM');
+	</script>
 </head>
+
 <body class="stretched no-transition">
